ff72ff2a7f28cfe2b10140f21c7bfd1d2e39d50f
Hayley Witherell <hayleywitherell@gmail.com>
2024-07-11 14:08:36 -0600

    Complete program 1

--- lessons/lesson_4/divisible.rb
+++ lessons/lesson_4/divisible.rb
@@ -1,5 +1,13 @@
 # Program 1
 
-# Write a method that returns in an array all the numbers between 1 and 100 that are divisible by 2 or 3 or 5. Then call the method in your program and print out what it returns.  Call the program `divisible.rb`.
+# Write a method that returns in an array all the numbers between 1 and 100 that are divisible by 2 or 3 or 5.
 
+def divisible
+  (1..100).select { |num|
+    num % 2 == 0 || num % 3 == 0 || num % 5 == 0
+  }
+end
 
+# Then call the method in your program and print out what it returns.  Call the program `divisible.rb`.
+
+print divisible()
